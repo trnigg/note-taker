4052edea3d330843f536fe98cd1e0f534d3eff92
trnigg <tim@nigg.com.au>
2023-11-06 10:09:54 +1100

    Added file/folder framework for modular API routing

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Require modularised API routes
+const api = require('./routes/index.js');
+
 // Middleware for parsing data types
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
