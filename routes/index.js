26c285e3febc971b37ffb12beec81ecb4de24005
trnigg <tim@nigg.com.au>
2023-11-06 10:53:03 +1100

    Created GET route for api/notes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,29 @@
-// File for routing
+// File for API routing
+const notesApi = require('express').Router();
+
+// for file system:
+const fs = require('fs');
+// for generating unique IDs: https://www.npmjs.com/package/uuid?activeTab=readme
+const { v4: uuidv4 } = require('uuid');
+
+
+// API GET route for notes
+
+notesApi.get('/api/notes', (req, res) => {
+    // read db.json
+    fs.readFile('./db/db.json')
+        .then((data) => {
+            // parse JSON data
+            let notes = JSON.parse(data);
+            // return JSON data
+            res.json(notes);
+        })
+        .catch((err) => {
+            //console log the error
+            console.error(err);
+            // return response status of 500 and a JSON object with key of 'error' and value of message matching 500 (Intern. Serv. Err)
+            res.status(500).json({ error : 'Internal Server Error' });
+        });
+});
+
+module.exports = notesApi;
