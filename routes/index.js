bc0c9256b90413ee622ff548e381cdc61e03e15b
trnigg <tim@nigg.com.au>
2023-11-06 11:03:37 +1100

    Specified character encoding and fixed 'fs' require for promises

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 // File for API routing
 const notesApi = require('express').Router();
 
-// for file system:
-const fs = require('fs');
+// for promise-based file system:
+const fs = require('fs').promises;
 // for generating unique IDs: https://www.npmjs.com/package/uuid?activeTab=readme
 const { v4: uuidv4 } = require('uuid');
 
@@ -11,18 +11,18 @@ const { v4: uuidv4 } = require('uuid');
 
 notesApi.get('/api/notes', (req, res) => {
     // read db.json
-    fs.readFile('./db/db.json')
+    fs.readFile('./db/db.json', 'utf-8')
         .then((data) => {
             // parse JSON data
             let notes = JSON.parse(data);
             // return JSON data
-            res.json(notes);
+            return res.json(notes);
         })
         .catch((err) => {
             //console log the error
             console.error(err);
             // return response status of 500 and a JSON object with key of 'error' and value of message matching 500 (Intern. Serv. Err)
-            res.status(500).json({ error : 'Internal Server Error' });
+            return res.status(500).json({ error : 'Internal Server Error' });
         });
 });
 
